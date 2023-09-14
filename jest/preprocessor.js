6906cf672b98da4c5cec80afbd084b004c261332
iqbal <muh.iqbal.st@gmail.com>
2023-09-14 12:56:26 +0700

    react native 0.63.3

--- jest/preprocessor.js
+++ jest/preprocessor.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -12,53 +12,42 @@
 
 'use strict';
 
-const metroBabelRegister = require('metro-babel-register');
-const nullthrows = require('nullthrows');
-const createCacheKeyFunction =
-  require('@jest/create-cache-key-function').default;
+const babelRegisterOnly = require('metro-babel-register');
+const createCacheKeyFunction = require('fbjs-scripts/jest/createCacheKeyFunction');
 
-const {
-  transformSync: babelTransformSync,
-  transformFromAstSync: babelTransformFromAstSync,
-} = require('@babel/core');
+const {transformSync: babelTransformSync} = require('@babel/core');
 const generate = require('@babel/generator').default;
 
-// Files matching this pattern will be transformed with the Node JS Babel
-// transformer, rather than with the React Native Babel transformer. Scripts
-// intended to run through Node JS should be included here.
-const nodeFiles = /[\\/]metro(?:-[^/]*)[\\/]/;
+const nodeFiles = new RegExp(
+  [
+    '/metro(?:-[^/]*)?/', // metro, metro-core, metro-source-map, metro-etc.
+  ].join('|'),
+);
+const nodeOptions = babelRegisterOnly.config([nodeFiles]);
 
-// Get Babel config from metro-babel-register, without registering a require
-// hook. This is used below to configure babelTransformSync under Jest.
-const {only: _, ...nodeBabelOptions} = metroBabelRegister.config([]);
+babelRegisterOnly([]);
 
-// Register Babel to allow the transformer itself to be loaded from source.
-require('../scripts/build/babel-register').registerForMonorepo();
-const transformer = require('@react-native/metro-babel-transformer');
-
+const transformer = require('metro-react-native-babel-transformer');
 module.exports = {
-  process(src /*: string */, file /*: string */) /*: {code: string, ...} */ {
+  process(src /*: string */, file /*: string */) /*: string */ {
     if (nodeFiles.test(file)) {
       // node specific transforms only
       return babelTransformSync(src, {
         filename: file,
         sourceType: 'script',
-        ...nodeBabelOptions,
+        ...nodeOptions,
         ast: false,
-      });
+      }).code;
     }
 
-    let {ast} = transformer.transform({
+    const {ast} = transformer.transform({
       filename: file,
       options: {
         ast: true, // needed for open source (?) https://github.com/facebook/react-native/commit/f8d6b97140cffe8d18b2558f94570c8d1b410d5c#r28647044
         dev: true,
         enableBabelRuntime: false,
         experimentalImportSupport: false,
-        globalPrefix: '',
-        hermesParser: true,
         hot: false,
-        // $FlowFixMe[incompatible-call] TODO: Remove when `inlineRequires` has been removed from metro-babel-transformer in OSS
         inlineRequires: true,
         minify: false,
         platform: '',
@@ -68,22 +57,48 @@ module.exports = {
         sourceType: 'unambiguous', // b7 required. detects module vs script mode
       },
       src,
-    });
-
-    const babelTransformResult = babelTransformFromAstSync(ast, src, {
-      ast: true,
-      retainLines: true,
       plugins: [
-        // TODO(moti): Replace with require('metro-transform-plugins').inlineRequiresPlugin when available in OSS
-        require('babel-preset-fbjs/plugins/inline-requires'),
+        [require('@babel/plugin-transform-block-scoping')],
+        // the flow strip types plugin must go BEFORE class properties!
+        // there'll be a test case that fails if you don't.
+        [require('@babel/plugin-transform-flow-strip-types')],
+        [
+          require('@babel/plugin-proposal-class-properties'),
+          // use `this.foo = bar` instead of `this.defineProperty('foo', ...)`
+          {loose: true},
+        ],
+        [require('@babel/plugin-transform-computed-properties')],
+        [require('@babel/plugin-transform-destructuring')],
+        [require('@babel/plugin-transform-function-name')],
+        [require('@babel/plugin-transform-literals')],
+        [require('@babel/plugin-transform-parameters')],
+        [require('@babel/plugin-transform-shorthand-properties')],
+        [require('@babel/plugin-transform-react-jsx')],
+        [require('@babel/plugin-transform-regenerator')],
+        [require('@babel/plugin-transform-sticky-regex')],
+        [require('@babel/plugin-transform-unicode-regex')],
+        [
+          require('@babel/plugin-transform-modules-commonjs'),
+          {strict: false, allowTopLevelThis: true},
+        ],
+        [require('@babel/plugin-transform-classes')],
+        [require('@babel/plugin-transform-arrow-functions')],
+        [require('@babel/plugin-transform-spread')],
+        [require('@babel/plugin-proposal-object-rest-spread')],
+        [
+          require('@babel/plugin-transform-template-literals'),
+          {loose: true}, // dont 'a'.concat('b'), just use 'a'+'b'
+        ],
+        [require('@babel/plugin-transform-exponentiation-operator')],
+        [require('@babel/plugin-transform-object-assign')],
+        [require('@babel/plugin-transform-for-of'), {loose: true}],
+        [require('@babel/plugin-transform-react-display-name')],
+        [require('@babel/plugin-transform-react-jsx-source')],
       ],
-      sourceType: 'module',
     });
-    ast = nullthrows(babelTransformResult.ast);
 
     return generate(
       ast,
-      // $FlowFixMe[prop-missing] Error found when improving flow typing for libs
       {
         code: true,
         comments: false,
@@ -94,13 +109,12 @@ module.exports = {
         sourceMaps: true,
       },
       src,
-    );
+    ).code;
   },
 
-  // $FlowFixMe[signature-verification-failure]
-  getCacheKey: createCacheKeyFunction([
+  getCacheKey: (createCacheKeyFunction([
     __filename,
-    require.resolve('@react-native/metro-babel-transformer'),
+    require.resolve('metro-react-native-babel-transformer'),
     require.resolve('@babel/core/package.json'),
-  ]),
+  ]) /*: any */),
 };
